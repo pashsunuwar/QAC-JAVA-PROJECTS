03ec1f799cf9735c5c9292da1c587d148d885cc5
pashsunuwar <psunuwar@qa.com>
2020-06-25 12:45:54 +0100

    added make tea java file#

---
+++ src/com/qa/systematicprogramming/MakeTea.java
@@ -0,0 +1,18 @@
+package com.qa.systematicprogramming;
+
+public class MakeTea {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Add water to tea bag");
+		System.out.println("Let the tea bag brew");
+		
+		Boolean milk = true;
+		
+		if (milk == true) {
+			System.out.println("Add milk & Serve tea");
+		}else {
+			System.out.println("Serve tea");
+		}
+	}
+}
